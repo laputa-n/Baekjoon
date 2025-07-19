83bc68ef023fededfbd19c5e563403536d07ea6e
laputa-n <128452330+laputa-n@users.noreply.github.com>
2025-07-20 01:47:55 +0900

    [Gold IV] Title: 작업, Time: 760 ms, Memory: 88912 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.java
+++ 백준/Gold/2056. 작업/작업.java
@@ -1,57 +1,52 @@
-import java.io.*;
-import java.util.*;
+    import java.io.*;
+    import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int[] time = new int[N+1];
-        int[] totalTime = new int[N+1];
-        ArrayList<Integer>[] before = new ArrayList[N+1];
-        ArrayList<Integer>[] after = new ArrayList[N+1];
-        int[] edgeCount = new int[N+1];
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i < N+1; i++) {
-            before[i] = new ArrayList<>();
-            after[i] = new ArrayList<>();
-        }
-        StringTokenizer st;
-        for(int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine()," ");
-            time[i] = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            for(int j=0; j<b; j++){
-                int w = Integer.parseInt(st.nextToken());
-                before[i].add(w);
-                after[w].add(i);
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            int N = Integer.parseInt(br.readLine());
+            int[] edgeCount = new int[N+1];
+            int[] time = new int[N+1];
+            int[] completeTime = new int[N+1];
+            ArrayList<Integer>[] adj = new ArrayList[N+1];
+            for (int i = 1; i <= N; i++) {
+                adj[i] = new ArrayList<>();
             }
-            edgeCount[i] += b;
-        }
-        for(int i = 1; i<=N; i++){
-            if(edgeCount[i] == 0)
-                q.add(i);
-        }
-        while(!q.isEmpty()){
-            int now = q.poll();
-            int m = 0;
-            for(int b:before[now]){
-                m = Math.max(totalTime[b],m);
+            StringTokenizer st;
+
+            for(int i = 1; i<=N; i++){
+                st = new StringTokenizer(br.readLine());
+                time[i] = Integer.parseInt(st.nextToken());
+                int prev = Integer.parseInt(st.nextToken());
+                edgeCount[i] = prev;
+                for(int j =0; j<prev; j++){
+                    adj[Integer.parseInt(st.nextToken())].add(i);
+                }
             }
-            totalTime[now] = m + time[now];
-            for(int a:after[now]){
-                edgeCount[a]--;
-                if(edgeCount[a] == 0)
-                    q.add(a);
+
+            Queue<Integer> q = new LinkedList<>();
+            for(int i = 1; i<=N; i++){
+                if(edgeCount[i] == 0){
+                    q.add(i);
+                }
             }
+            while(!q.isEmpty()){
+                int cur = q.poll();
+                completeTime[cur] += time[cur];
+                for(int x: adj[cur]){
+                    completeTime[x] = Math.max(completeTime[x], completeTime[cur]);
+                    if(--edgeCount[x] == 0)
+                        q.add(x);
+                }
+            }
+            int max = 0;
+            for(int i = 1; i<=N; i++){
+                max = Math.max(max, completeTime[i]);
+            }
+            bw.write(String.valueOf(max));
+            bw.flush();
+            bw.close();
+            br.close();
         }
-        int result = 0;
-        for(int t: totalTime){
-            result = Math.max(result,t);
-        }
-        bw.write(String.valueOf(result));
-        bw.flush();
-        bw.close();
-        br.close();
     }
-}
