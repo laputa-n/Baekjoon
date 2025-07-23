0e408c705e53c6c0d8f60ffaab805e842a1260e8
laputa-n <128452330+laputa-n@users.noreply.github.com>
2025-07-23 15:56:47 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 124 ms, Memory: 16404 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -1,16 +1,18 @@
+import org.w3c.dom.Node;
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
+    static int[] dx = {1,0,-1,0};
+    static int[] dy = {0,1,0,-1};
     static int N,M;
     static char[][] board;
     static Node start;
     static int bfs(){
         Queue<Node> q = new LinkedList<>();
         boolean[][][] visited = new boolean[N][M][1<<6];
-        q.add(start);
+        q.offer(start);
         visited[start.row][start.col][0] = true;
         while(!q.isEmpty()){
             Node cur = q.poll();
@@ -25,21 +27,22 @@ public class Main {
 
                 if(board[nextRow][nextCol] >= 'a' && board[nextRow][nextCol] <= 'f'){
                     int nextKey = 1 << (board[nextRow][nextCol] - 'a');
-                    nextKey = cur.key|nextKey;
-                    visited[nextRow][nextCol][cur.key] = true;
+                    nextKey = nextKey|cur.key;
+                    visited[nextRow][nextCol][nextKey] = true;
                     q.offer(new Node(nextRow,nextCol,cur.dist+1,nextKey));
-                } else if(board[nextRow][nextCol] >= 'A' && board[nextRow][nextCol] <= 'F'){
-                    if((cur.key&(1<< (board[nextRow][nextCol] - 'A'))) != 0){
-                        visited[nextRow][nextCol][cur.key] = true;
-                        q.offer(new Node(nextRow,nextCol,cur.dist+1,cur.key));
-                    }
+                } else if (board[nextRow][nextCol]>='A' && board[nextRow][nextCol]<='F'){
+                    if((cur.key& (1<<(board[nextRow][nextCol]-'A'))) == 0) continue;
+                    visited[nextRow][nextCol][cur.key] = true;
+                    q.offer(new Node(nextRow,nextCol,cur.dist+1,cur.key));
                 } else {
                     visited[nextRow][nextCol][cur.key] = true;
                     q.offer(new Node(nextRow,nextCol,cur.dist+1,cur.key));
                 }
             }
         }
+
         return -1;
+
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -48,16 +51,15 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         board = new char[N][M];
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < M; j++) {
-                board[i][j] = str.charAt(j);
+        for(int i = 0; i < N; i++){
+            String line = br.readLine();
+            for(int j = 0; j < M; j++){
+                board[i][j] = line.charAt(j);
                 if(board[i][j] == '0')
                     start = new Node(i,j,0,0);
             }
         }
-        int ans = bfs();
-        bw.write(String.valueOf(ans));
+        bw.write(String.valueOf(bfs()));
         bw.flush();
         bw.close();
         br.close();
