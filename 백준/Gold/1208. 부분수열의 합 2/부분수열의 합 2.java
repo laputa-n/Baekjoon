00d94d1b437b9840957ebf08affd277094007e52
laputa-n <128452330+laputa-n@users.noreply.github.com>
2025-08-07 17:37:35 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 1124 ms, Memory: 35364 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.java
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.java
@@ -10,64 +10,58 @@ public class Main {
         int S = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for(int i = 0; i<N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
         int size = N/2;
-        int[] a = new int[1<<(N-size)];
-        int[] b= new int[1<<(size)];
-        for(int i = 0; i<(1<<(N-size)); i++){
-            for(int j = 0; j<(N-size); j++){
+        int[] a = new int[(1<<size)];
+        int[] b = new int[(1<<(N-size))];
+        for(int i = 0; i<(1<<size); i++){
+            for(int j = 0; j<size; j++){
                 if(((1<<j) & i) != 0){
                     a[i] += arr[j];
                 }
             }
         }
-        for(int i = 0; i<(1<<size); i++){
-            for(int j = 0; j<size; j++){
+        for(int i = 0; i<(1<<(N-size)); i++){
+            for(int j =0; j<(N-size); j++){
                 if(((1<<j) & i) != 0){
-                    b[i] += arr[j + (N-size)];
+                    b[i] += arr[size + j];
                 }
             }
         }
         Arrays.sort(a);
         Arrays.sort(b);
+
         long cnt = 0;
-        for(int i =0; i<a.length; i++){
-            int av = a[i];
-            int aTerm = upperBound(a,av) - lowerBound(a,av);
-            int bTerm = upperBound(b,S-av) - lowerBound(b,S-av);
-            cnt += (long) aTerm *bTerm;
-            i += (aTerm-1);
+        int ap = 0;
+        int bp = b.length-1;
+        while(ap < a.length && bp>= 0){
+            int av = a[ap];
+            int bv = b[bp];
+
+            if(av+bv == S){
+                long ac = 0;
+                long bc = 0;
+                while(ap < a.length && av == a[ap]){
+                    ac++;
+                    ap++;
+                }
+                while(bp >= 0 && bv == b[bp]){
+                    bc++;
+                    bp--;
+                }
+                cnt += ((long) ac * bc);
+            } else if (av+bv < S){
+                ap++;
+            } else if (av+bv>S){
+                bp--;
+            }
         }
-        if(S == 0)
-            cnt--;
+        if(S == 0) cnt--;
         bw.write(String.valueOf(cnt));
         bw.flush();
         bw.close();
         br.close();
     }
-    static int upperBound(int[] arr, int t){
-        int left = 0; int right = arr.length;
-        while(left < right){
-            int mid = (left + right)/2;
-            if(arr[mid] <= t){
-                left = mid + 1;
-            } else
-                right = mid;
-        }
-        return right;
-    }
-    static int lowerBound(int[] arr,int t){
-        int left = 0; int right = arr.length;
-        while(left < right){
-            int mid = (left + right)/2;
-            if(arr[mid] < t){
-                left = mid + 1;
-            } else
-                right = mid;
-        }
-        return right;
-    }
-
 }
