f86c14cd3cbc2060d119f80a7616e0d0273762cb
laputa-n <128452330+laputa-n@users.noreply.github.com>
2025-09-04 12:49:21 +0900

    [Gold V] Title: 캠프 준비, Time: 124 ms, Memory: 15208 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.java
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.java
@@ -18,18 +18,25 @@ public class Main{
 
         int cnt = 0;
         for(int i = 0; i<1<<N; i++){
+            boolean flag = false;
             int sum = 0;
             int min = Integer.MAX_VALUE;
             int max = Integer.MIN_VALUE;
             for(int j = 0; j<N; j++){
                 if((i&(1<<j)) != 0){
+                    if(sum > R){
+                        flag = true;
+                        break;
+                    }
                     sum += level[j];
                     min = Math.min(min, level[j]);
                     max = Math.max(max, level[j]);
                 }
             }
-            if(sum >= L && sum <= R && max-min >= X)
-                cnt++;
+            if(!flag){
+                if(sum >= L && sum <= R && max-min >= X)
+                    cnt++;
+            }
         }
         bw.write(String.valueOf(cnt));
         bw.flush();
