8950d41d857bd1725888970e24d269066763d107
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-01-08 15:00:10 +0900

    [Gold IV] Title: 줄세우기, Time: 100 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.java
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,23 +6,34 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
+        int[] dp = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        int[] dp = new int[N];
-        Arrays.fill(dp, 1);
-        int lis = 1;
-        for(int i = 1; i < N; i++) {
-            for(int j = 0; j < i; j++) {
-                if(arr[i]>arr[j]){
-                    dp[i] = Math.max(dp[i], dp[j]+1);
-                }
+        int last = 0;
+        dp[0] = arr[0];
+        for(int i = 1; i<N; i++){
+            if(arr[i] > dp[last]){
+                dp[++last] = arr[i];
+            } else {
+                int idx = lowerBound(0,last,arr[i],dp);
+                dp[idx] = arr[i];
             }
-            lis = Math.max(lis,dp[i]);
         }
-        bw.write(String.valueOf(N-lis));
+        bw.write(String.valueOf(N-last-1));
         bw.flush();
         bw.close();
         br.close();
+    }
+    static int lowerBound(int start, int end, int key, int[] ar){
+        while(start<end){
+            int mid = (start+end)/2;
+            if(ar[mid]<key){
+                start = mid+1;
+            } else {
+                end = mid;
+            }
+        }
+        return end;
     }
 }
