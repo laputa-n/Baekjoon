e454c08fd6907297f1f83ce208c024ba3501b161
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-01-13 14:26:57 +0900

    [Gold III] Title: 개구리 점프, Time: 876 ms, Memory: 91248 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17619. 개구리 점프/개구리 점프.java
+++ 백준/Gold/17619. 개구리 점프/개구리 점프.java
@@ -12,6 +12,7 @@ public class Main {
     static void union(int a,int b){
         int rootA = find(a);
         int rootB = find(b);
+        if(rootA == rootB) return;
         if(rootA < rootB){
             parent[rootB] = rootA;
         } else {
@@ -32,35 +33,48 @@ public class Main {
         }
         for(int i = 1; i <= N; i++){
             String[] line = br.readLine().split(" ");
-            logs[i] = new log(Integer.parseInt(line[0]),Integer.parseInt(line[1]),Integer.parseInt(line[2]));
-            for(int j = 1; j<i; j++){
-                if(logs[i].x1>logs[j].x2 || logs[i].x2<logs[j].x1) continue;
-                union(i,j);
+            logs[i] = new log(Integer.parseInt(line[0]),Integer.parseInt(line[1]),Integer.parseInt(line[2]), i);
+        }
+
+        Arrays.sort(logs,1,N+1, (a,b) -> {
+            if(a.x1 != b.x1) return Integer.compare(a.x1, b.x1);
+            return Integer.compare(a.x2, b.x2);
+        });
+
+        int rep = 1;
+        int curRight = logs[1].x2;
+
+        for(int i = 2; i <= N; i++){
+            if(curRight>=logs[i].x1){
+                union(logs[i].id,logs[rep].id);
+                curRight = Math.max(curRight,logs[i].x2);
+            } else {
+                rep = i;
+                curRight = logs[i].x2;
             }
         }
-//        for(int i = 1; i <= N; i++){
-//            bw.write(String.valueOf(i) + " " + String.valueOf(find(i))+"\n");
-//        }
         while(Q-->0){
             String[] line = br.readLine().split(" ");
             int a = Integer.parseInt(line[0]);
             int b = Integer.parseInt(line[1]);
-            if(find(a) != find(b)){
-                bw.write("0\n");
-            } else {
+            if(find(a) == find(b)){
                 bw.write("1\n");
+            } else {
+                bw.write("0\n");
             }
         }
+
         bw.flush();
         bw.close();
         br.close();
     }
     static class log{
-        int x1, x2, y;
-        public log(int x1, int x2, int y){
+        int x1, x2, y, id;
+        public log(int x1, int x2, int y, int id){
             this.x1 = x1;
             this.x2 = x2;
             this.y = y;
+            this.id = id;
         }
     }
 }
