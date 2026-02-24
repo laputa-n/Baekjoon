abfc7feca750867b8d781b6fc486b004db46a9d6
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-02-24 17:39:46 +0900

    [level 1] Title: 체육복, Time: 1.01 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        int answer = n - lost.length;
+        for(int i = 0 ; i<lost.length; i++){
+            for(int j = 0; j<reserve.length; j++){
+                if(lost[i] == reserve[j]){
+                    answer++;
+                    lost[i] = - 1;
+                    reserve[j] = -1;
+                }
+            }
+        }
+
+        
+
+        for(int i =0; i<lost.length; i++){
+            for(int j = 0; j<reserve.length; j++){
+                if(reserve[j]+1 == lost[i] || reserve[j]-1 == lost[i]){
+                    answer++;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
