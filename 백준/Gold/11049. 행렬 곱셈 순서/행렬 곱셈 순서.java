ce33435a7f485d02b6fa720877803d20e7603433
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-02-26 16:52:26 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 236 ms, Memory: 18884 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -2,33 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static final int INF = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int[] r = new int[N+1];
+        int n = Integer.parseInt(br.readLine());
+        int[] data = new int[n+1];
         StringTokenizer st;
-        for (int i = 0; i < N; i++) {
+        for(int i =0; i<n;i++){
             st = new StringTokenizer(br.readLine());
-            r[i] = Integer.parseInt(st.nextToken());
-            r[i+1] = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            data[i] = a; data[i+1] = b;
         }
-        int[][] dp = new int[N][N];
-        for(int i =0; i<N-1; i++){
-            dp[i][i+1] = r[i] * r[i+1] * r[i+2];
-        }
-
-        for(int i = 2; i<N; i++){
-            for(int j = 0; j+i < N; j++){
-                dp[j][j+i] = Integer.MAX_VALUE;
-                for(int k = j; k<i+j; k++){
-                    int temp = dp[j][k] + dp[k+1][j+i] + r[j] * r[k+1] * r[j+i+1];
-                    dp[j][j+i] = Math.min(dp[j][j+i], temp);
+        int[][] dp = new int[n][n];
+        for(int i = 2; i <=n; i++){ //간격
+            for(int j = 0; j<n+1-i; j++){
+                dp[j][j+i-1] = INF;
+                for(int k = j; k<j+i-1; k++){
+                    int val = dp[j][k] + dp[k+1][j+i-1] + (data[j]*data[k+1]*data[j+i]);
+                    dp[j][j+i-1] = Math.min(val, dp[j][j+i-1]);
                 }
             }
         }
-        int ans = dp[0][N-1];
-        bw.write(String.valueOf(ans));
+        bw.write(String.valueOf(dp[0][n-1]));
         bw.flush();
         bw.close();
         br.close();
