379bb65db23bf2524c14e6409206a75331c97c4e
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-02-27 00:50:19 +0900

    [Gold III] Title: ACM Craft, Time: 712 ms, Memory: 244788 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -6,57 +6,50 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        for(int i = 0; i<T; i++){
-            st = new StringTokenizer(br.readLine()," ");
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
-            int[] time = new int[N+1];
-            int[] totalTime = new int[N+1];
-            int[] edgeCount = new int[N+1];
-            ArrayList<Integer>[] to = new ArrayList[N+1];
-            ArrayList<Integer>[] from = new ArrayList[N+1];
+            List<Integer>[] nextList = new List[N+1];
+            for(int i = 0; i <= N; i++){
+                nextList[i] = new ArrayList<>();
+            }
+            int[] dp = new int[N+1];
+            dp[0] = 0;
+            int[] indegree = new int[N+1];
+            Arrays.fill(indegree, 0);
+            int[] times = new int[N+1];
+            st = new StringTokenizer(br.readLine());
+            for(int i = 1; i <= N; i++) {
+                times[i] = Integer.parseInt(st.nextToken());
+            }
+            while(K-- > 0) {
+                st = new StringTokenizer(br.readLine());
+                int X = Integer.parseInt(st.nextToken());
+                int Y = Integer.parseInt(st.nextToken());
+                nextList[X].add(Y);
+                indegree[Y]++;
+            }
+            int tar = Integer.parseInt(br.readLine());
             Queue<Integer> q = new LinkedList<>();
-
-            for(int j = 0; j<=N; j++){
-                to[j] = new ArrayList<>();
-            }
-            for(int j = 0; j<=N; j++){
-                from[j] = new ArrayList<>();
-            }
-            st = new StringTokenizer(br.readLine()," ");
-            for(int j = 1; j<=N; j++){
-                time[j] = Integer.parseInt(st.nextToken());
-            }
-            for(int j =0; j<K; j++){
-                st = new StringTokenizer(br.readLine()," ");
-                int u = Integer.parseInt(st.nextToken());
-                int v = Integer.parseInt(st.nextToken());
-                to[u].add(v);
-                from[v].add(u);
-                edgeCount[v]++;
-            }
-            for(int j = 1; j<=N; j++){
-                if(edgeCount[j] == 0){
-                    q.add(j);
+            for(int i = 1; i <= N; i++) {
+                if(indegree[i] == 0) {
+                    q.add(i);
+                    dp[i] = times[i];
                 }
             }
-            while(!q.isEmpty()){
-                int u = q.poll();
-                int maxTime = 0;
-                for(int x: from[u]){
-                    maxTime = Math.max(maxTime,totalTime[x]);
-                }
-                totalTime[u] = maxTime+time[u];
-                for(int x:to[u]){
-                    edgeCount[x]--;
-                    if(edgeCount[x] == 0){
-                        q.add(x);
+            while(!q.isEmpty()) {
+                int cur = q.poll();
+                if(cur == tar) break;
+                for(int n: nextList[cur]) {
+                    dp[n] = Math.max(dp[n], dp[cur] + times[n]);
+                    indegree[n]--;
+                    if(indegree[n] == 0) {
+                        q.add(n);
                     }
                 }
             }
-            int tar = Integer.parseInt(br.readLine());
-            bw.write(String.valueOf(totalTime[tar])+"\n");
+            bw.write(String.valueOf(dp[tar]) + "\n");
         }
         bw.flush();
         bw.close();
