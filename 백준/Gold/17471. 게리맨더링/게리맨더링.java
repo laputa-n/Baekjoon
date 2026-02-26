255ae3ec78c0ef48a2ce696dcb0125252af7e4d9
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-02-27 01:48:24 +0900

    [Gold III] Title: 게리맨더링, Time: 128 ms, Memory: 15724 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -2,79 +2,100 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int min = Integer.MAX_VALUE;
-    static int[] people;
-    static int N;
-    static List<Integer>[] adj;
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        N = Integer.parseInt(br.readLine());
-        people = new int[N+1];
-        String[] input = br.readLine().split(" ");
-        for(int i = 1; i <= N; i++) {
-            people[i] = Integer.parseInt(input[i-1]);
+        int N = Integer.parseInt(br.readLine());
+        int[] people= new int[N+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 1; i<=N; i++){
+            people[i] = Integer.parseInt(st.nextToken());
         }
-        adj = new List[N+1];
-        for(int i = 1; i <= N; i++) {
+        List<Integer>[] adj = new List[N+1];
+        for(int i = 1; i<=N; i++){
             adj[i] = new ArrayList<>();
         }
-        for(int i = 1; i <= N; i++) {
-            input = br.readLine().split(" ");
-            int m = Integer.parseInt(input[0]);
-            for(int j = 1; j<=m; j++){
-                int n = Integer.parseInt(input[j]);
-                adj[i].add(n);
+        for(int i = 1; i<=N; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            for(int j = 0; j<a; j++){
+                adj[i].add(Integer.parseInt(st.nextToken()));
             }
         }
-        int l = 1<<N;
-        for(int i = 1; i <l-1; i++){
+        int diff = 10000;
+        for(int i = 1; i<((1<<N)-1); i++){
             List<Integer> red = new ArrayList<>();
             List<Integer> blue = new ArrayList<>();
             for(int j = 0; j<N; j++){
-                if((i&(1<<j)) == 0)
+                if(((1<<j)&i)!=0){
                     red.add(j+1);
-                else
-                    blue.add(j+1);
-            }
-            if(isConnected(red) && isConnected(blue)){
-                int redTotal = 0;
-                for(int r:red){
-                    redTotal += people[r];
                 }
-                int blueTotal = 0;
-                for(int b:blue){
-                    blueTotal += people[b];
-                }
-                min = Math.min(min, Math.abs(redTotal-blueTotal));
             }
+            for(int j = 1; j<=N; j++){
+                if(!red.contains(j)){
+                    blue.add(j);
+                }
+            }
+
+            boolean[] visited = new boolean[N+1];
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(red.get(0));
+            visited[red.get(0)] = true;
+            while(!queue.isEmpty()){
+                int cur = queue.poll();
+                for(int n:adj[cur]){
+                    if(!visited[n] && red.contains(n)){
+                        visited[n] = true;
+                        queue.add(n);
+                    }
+                }
+            }
+            boolean isConnected = true;
+            for(int n:red){
+                if(!visited[n]){
+                    isConnected = false;
+                    break;
+                }
+            }
+            if(!isConnected) continue;
+
+            queue = new LinkedList<>();
+            queue.add(blue.get(0));
+            visited[blue.get(0)] = true;
+            while(!queue.isEmpty()){
+                int cur = queue.poll();
+                for(int n:adj[cur]){
+                    if(!visited[n] && blue.contains(n)){
+                        visited[n] = true;
+                        queue.add(n);
+                    }
+                }
+            }
+
+            isConnected = true;
+            for(int n:blue){
+                if(!visited[n]){
+                    isConnected = false;
+                    break;
+                }
+            }
+            if(!isConnected) continue;
+
+
+            int redPeopleCnt = 0;
+            int bluePeopleCnt = 0;
+            for(int n:red){
+                redPeopleCnt+=people[n];
+            }
+            for(int n:blue){
+                bluePeopleCnt+=people[n];
+            }
+            diff= Math.min(diff,Math.abs(redPeopleCnt - bluePeopleCnt));
+            if(diff == 0) break;
         }
-        if(min == Integer.MAX_VALUE)
-            bw.write("-1");
-        else bw.write(String.valueOf(min));
+        bw.write(diff==10000?"-1":String.valueOf(diff)+"\n");
         bw.flush();
         bw.close();
         br.close();
-    }
-    static boolean isConnected(List<Integer> list) {
-        boolean[] visited = new boolean[N+1];
-        int start = list.get(0);
-        Deque<Integer> q = new ArrayDeque<>();
-        q.add(start);
-        visited[start] = true;
-        while(!q.isEmpty()){
-            int cur = q.poll();
-            for(int nxt:adj[cur]){
-                if(!visited[nxt] && list.contains(nxt)){
-                    visited[nxt] = true;
-                    q.add(nxt);
-                }
-            }
-        }
-        for(int li:list){
-            if(!visited[li])
-                return false;
-        }
-        return true;
     }
 }
