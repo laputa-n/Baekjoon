6d8540bb125769c7416a859f8f4fa8f552c7efff
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-02-27 19:07:30 +0900

    [Gold V] Title: 평범한 배낭, Time: 172 ms, Memory: 54156 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,27 +1,31 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[] W = new int[N+1];
-        int[] V = new int[N+1];
-        for(int i = 1; i <= N; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            W[i] = Integer.parseInt(st.nextToken());
-            V[i] = Integer.parseInt(st.nextToken());
+        Item[] items = new Item[N+1];
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            items[i+1] = new Item(w, v);
         }
+        items[0] = new Item(0,0);
+        Arrays.sort(items);
         int[][] dp = new int[N+1][K+1];
         for(int i = 1; i<=N; i++){
             for(int j = 1; j<=K; j++){
-                if(W[i] > j){
-                    dp[i][j] = dp[i-1][j];
+                if(items[i].weight <= j){
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-items[i].weight]+items[i].value);
                 } else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-W[i]] + V[i]);
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
@@ -30,7 +34,16 @@ public class Main {
         bw.close();
         br.close();
     }
+    static class Item implements Comparable<Item>{
+        int weight, value;
+        public Item(int weight, int value){
+            this.weight = weight;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Item o) {
+            return Integer.compare(weight, o.weight);
+        }
+    }
 }
-
-
-
