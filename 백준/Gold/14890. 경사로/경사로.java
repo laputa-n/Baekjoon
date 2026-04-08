c220e2287c6bc62b3eb981f37c45de3294bf1079
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-04-08 17:02:33 +0900

    [Gold III] Title: 경사로, Time: 124 ms, Memory: 15260 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.java
+++ 백준/Gold/14890. 경사로/경사로.java
@@ -1,104 +1,122 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
+public class Main{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
-        int[][] map = new int[N][N];
-        boolean[][][] checked = new boolean[N][N][2];
-        for(int i = 0; i < N; i++) {
-            String[] input = br.readLine().split(" ");
-            for(int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(input[j]);
+        int[][] matrix = new int[N][N];
+        for(int i = 0; i<N; i++){
+            String[] lines = br.readLine().split(" ");
+            for(int j = 0; j<N; j++){
+                matrix[i][j] = Integer.parseInt(lines[j]);
             }
         }
-        int cnt = 0;
-        for(int i = 0; i < N; i++) {
-            boolean isPass = true;
-            for(int j = 0; j < N-1; j++) {
-                if(isPass){
-                    if(map[i][j+1] > map[i][j]){
-                        if(j-L+1<0){
-                            isPass = false;
-                            break;
-                        }
-                        for(int k = 0; k<L; k++){
-                            if(map[i][j-k] != (map[i][j+1] - 1) || checked[i][j-k][0]){
-                                isPass = false;
-                                break;
-                            }
-                        }
-                        for(int k = 0; k<L; k++){
-                            checked[i][j-k][0] = true;
-                        }
-                    } else if(map[i][j+1] < map[i][j]) {
-                        if(j+L > N-1){
-                            isPass = false;
-                            break;
-                        }
-                        for(int k = 0; k<L; k++){
-                            if(map[i][j+k+1]!= (map[i][j]-1) || checked[i][j+k+1][0]){
-                                isPass = false;
-                                break;
-                            }
-                        }
-                        for(int k = 0; k<L; k++){
-                            checked[i][j+k+1][0] = true;
-                        }
-                        j+=(L-1);
-                    }
-                } else break;
-            }
-            if(isPass) cnt++;
-        }
-
-        for(int i = 0; i < N; i++) {
-            boolean isPass = true;
-            for(int j = 0; j < N-1; j++) {
-                if(isPass){
-                    if(map[j+1][i] > map[j][i]){
-                        if(j-L+1<0){
-                            isPass = false;
-                            break;
-                        }
-                        for(int k = 0; k<L; k++){
-                            if(map[j-k][i] != (map[j+1][i] - 1) || checked[j-k][i][1]){
-                                isPass = false;
-                                break;
-                            }
-                        }
-                        for(int k = 0; k<L; k++){
-                            checked[j-k][i][1] = true;
-                        }
-                    } else if(map[j+1][i] < map[j][i]) {
-                        if(j+L > N-1){
-                            isPass = false;
-                            break;
-                        }
-                        for(int k = 0; k<L; k++){
-                            if(map[j+k+1][i]!= (map[j][i]-1) || checked[j+k+1][i][1]){
-                                isPass = false;
-                                break;
-                            }
-                        }
-                        for(int k = 0; k<L; k++){
-                            checked[j+k+1][i][1] = true;
-                        }
-                        j+=(L-1);
-                    }
-                } else break;
-            }
-            if(isPass) cnt++;
-        }
-
-        bw.write(String.valueOf(cnt));
+        bw.write(String.valueOf(solution(N,L,matrix)));
         bw.flush();
         bw.close();
         br.close();
+    }
+    static int solution(int N, int L, int[][] matrix){
+        int cnt = 0;
+        for(int i = 0; i<N; i++){
+            boolean[] chk = new boolean[N];
+            for(int j = 0; j<N; j++){
+                if(j >= N-1){
+                    cnt++;
+                    break;
+                }
+                int diff = matrix[i][j] - matrix[i][j+1];
+                if(Math.abs(diff)>1) break;
+                if(diff < 0){
+                    boolean flag = true;
+                    for(int k = j; k>j-L; k--){
+                        if(k<0){
+                            flag = false;
+                            break;
+                        }
+                        if(chk[k] || matrix[i][j] != matrix[i][k]){
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(flag){
+                        for(int k = j; k>j-L; k--){
+                            chk[k] = true;
+                        }
+                    } else break;
+                } else if(diff > 0){
+                    boolean flag = true;
+                    for(int k = j+1; k<=j+L; k++){
+                        if(k>N-1){
+                            flag = false;
+                            break;
+                        }
+                        if(chk[k] || matrix[i][j]-1 != matrix[i][k]){
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(flag){
+                        for(int k = j+1; k<=j+L; k++){
+                            chk[k] = true;
+                        }
+                        j = j+L-1;
+                    } else break;
+                }
+            }
+        }
 
+        for(int i = 0; i<N; i++){
+            boolean[] chk = new boolean[N];
+            for(int j = 0; j<N; j++){
+                if(j >= N-1){
+                    cnt++;
+                    break;
+                }
+                int diff = matrix[j][i] - matrix[j+1][i];
+                if(Math.abs(diff)>1) break;
+                if(diff < 0){
+                    boolean flag = true;
+                    for(int k = j; k>j-L; k--){
+                        if(k<0){
+                            flag = false;
+                            break;
+                        }
+                        if(chk[k] || matrix[j][i] != matrix[k][i]){
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(flag){
+                        for(int k = j; k>j-L; k--){
+                            chk[k] = true;
+                        }
+                    } else break;
+                } else if(diff > 0){
+                    boolean flag = true;
+                    for(int k = j+1; k<=j+L; k++){
+                        if(k>N-1){
+                            flag = false;
+                            break;
+                        }
+                        if(chk[k] || matrix[j][i]-1 != matrix[k][i]){
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(flag){
+                        for(int k = j+1; k<=j+L; k++){
+                            chk[k] = true;
+                        }
+                        j = j+L-1;
+                    } else break;
+                }
+            }
+        }
+        return cnt;
     }
 }
