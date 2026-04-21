75068a78a476331c90f77a6f0f993a17058fd81d
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-04-21 18:21:06 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 94.66 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,18 +2,23 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String,Integer> map = new HashMap<>();
-        for(int i = 0; i<participant.length; i++){
-            map.put(participant[i], map.getOrDefault(participant[i],0) + 1);
+        
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String s: participant){
+            map.put(s, map.getOrDefault(s,0) + 1);
         }
-        for(int i = 0; i<completion.length; i++){
-            if(map.get(completion[i]) == 1){
-                map.remove(completion[i]);
-            } else {
-                map.replace(completion[i], map.get(completion[i]) - 1);
+        
+        for(String s: completion){
+            map.put(s,map.get(s) - 1);
+        }
+        
+        for(String s:map.keySet()){
+            if(map.get(s) != 0){
+                answer = s;
+                break;
             }
         }
-        String answer = map.keySet().iterator().next();
         return answer;
     }
 }
