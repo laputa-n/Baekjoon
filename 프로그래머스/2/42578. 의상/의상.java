42c3271b62473be6439d0808bd29a6c8d578762b
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-04-22 13:38:50 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,14 +3,15 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         HashMap<String, Integer> map = new HashMap<>();
-        for (String[] cloth : clothes) {
-            map.put(cloth[1], map.getOrDefault(cloth[1],0) + 1);
+        for(String[] tar:clothes){
+            String name = tar[0];
+            String type = tar[1];
+            map.put(type,map.getOrDefault(type,0)+1);
         }
-        int ans = 1;
-        for(String key: map.keySet()) {
-            ans *= (map.get(key)+1);
+        int cnt = 1;
+        for(String key:map.keySet()){
+            cnt *= (map.get(key)+1);
         }
-
-        return ans - 1;
+        return cnt-1;
     }
 }
