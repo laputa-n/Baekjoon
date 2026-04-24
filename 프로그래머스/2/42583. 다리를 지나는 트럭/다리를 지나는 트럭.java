86467ffe9599c2292a5d27e7b52a57a627b80998
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-04-24 15:43:53 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 11.13 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,30 +1,38 @@
+import java.util.*;
+
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int passed = 0;
-        int nextNum = 0;
-        int[] bridge = new int[bridge_length];
-        int time = 0;
-        while(passed<truck_weights.length){
-            int weightsOnBridge = 0;
-            for(int i = bridge.length-1; i>=0; i--){
-                if(i == bridge_length - 1){
-                    if(bridge[i] != 0){
-                        bridge[i] = 0;
-                        passed++;
-                    }
-                } else {
-                    bridge[i+1] = bridge[i];
-                    bridge[i] = 0;
-                    weightsOnBridge += bridge[i+1];
-                }
-            }
-            if(!(nextNum>=truck_weights.length)){
-                if(weight - weightsOnBridge >= truck_weights[nextNum]){
-                    bridge[0] = truck_weights[nextNum++];
-                }
-            }
+        //0: 시간, 1: 트럭 번호
+        ArrayDeque<int[]> bridge = new ArrayDeque<>();
+        
+        //time: 현재 시각, next: 다음 트럭 번호, total_weights: 다리 위 무게 총합
+        int time = 1;
+        int next = 1;
+        int total_weights = 0;
+        
+        bridge.offer(new int[]{1,0});
+        total_weights += truck_weights[0];
+        
+        while(!bridge.isEmpty()){
             time++;
+            //맨 앞
+            int[] head = bridge.peek();
+            //1. 현재 시간 기준 다 건넜으면, 다리 위에서 제거
+            if(time - head[0] >= bridge_length){
+                bridge.poll();
+                total_weights -= truck_weights[head[1]];
+            }
+            
+            //2. 다음 차가 다리 위 진입할 수 있으면, 넣어주기
+            if(next >= truck_weights.length) continue;
+            
+            if(total_weights + truck_weights[next] <= weight){
+                bridge.offer(new int[]{time, next});
+                total_weights += truck_weights[next];
+                next++;
+            }
         }
+        
         return time;
-    }
+    }        
 }
