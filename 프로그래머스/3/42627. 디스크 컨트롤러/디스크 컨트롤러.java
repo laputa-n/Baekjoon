19005d4f4d3c23681e75d3a7cf7c8b236970ff89
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-04-26 12:24:52 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.06 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,32 +1,30 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
-        Arrays.sort(jobs, (a,b) -> Integer.compare(a[0],b[0]));
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[1],b[1]));
-
-        int n = jobs.length;
-        int idx = 0;
+        Arrays.sort(jobs, (j1,j2) -> j1[0] - j2[0]);
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((j1,j2) -> j1[1] - j2[1]);
         int time = 0;
-        long sum = 0;
-        int done = 0;
-        while(done<n){
-            while(idx < n && jobs[idx][0] <= time){
-                pq.offer(jobs[idx]);
-                idx++;
+        int idx = 0;
+        int total = 0;
+        int cnt = 0;
+        
+        while(cnt < jobs.length){
+            
+            while(idx < jobs.length && jobs[idx][0] <= time){
+                pq.add(jobs[idx++]);
             }
-
+            
             if(pq.isEmpty()){
                 time = jobs[idx][0];
-                continue;
+            } else {
+                int[] job = pq.poll();
+                time += job[1];
+                total += (time - job[0]);
+                cnt++;
             }
-
-            int[] cur = pq.poll();
-            time += cur[1];
-            sum +=  time - cur[0];
-            done++;
         }
-
-        return (int)sum/n;
+        
+        return total/jobs.length;
     }
 }
