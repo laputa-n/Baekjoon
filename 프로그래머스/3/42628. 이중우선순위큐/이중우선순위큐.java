5cd0ea71050dac8b66b37edd77f10387cd12d301
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-04-26 12:41:19 +0900

    [level 3] Title: 이중우선순위큐, Time: 2117.96 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,26 +2,51 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-        String[] oper;
-        for(String operation:operations){
-            oper = operation.split(" ");
-            if(oper[0].equals("I")){
-                int num = Integer.parseInt(oper[1]);
-                map.put(num,map.getOrDefault(num,0) + 1);
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(String s: operations){
+            String[] operation = s.split(" ");
+            String cmd = operation[0];
+            String number = operation[1];
+            
+            if(cmd.equals("I")){
+                pq.add(Integer.parseInt(number));
             } else {
-                if(map.isEmpty()) continue;
-                int key = oper[1].equals("1")?map.lastKey():map.firstKey();
-                int cnt = map.get(key);
-                if(cnt == 1){
-                    map.remove(key);
+                if(pq.isEmpty()) continue;
+                
+                if(number.equals("1")){
+                    PriorityQueue<Integer> pq2 = new PriorityQueue<>((o1,o2) -> o2 - o1);
+                    
+                    while(!pq.isEmpty()){
+                        pq2.add(pq.poll());
+                    }
+                    
+                    pq2.poll();
+                    
+                    while(!pq2.isEmpty()){
+                        pq.add(pq2.poll());
+                    }
+                    
                 } else {
-                    map.put(key,cnt-1);
+                    pq.poll();
                 }
             }
         }
-
-        if(map.isEmpty()) return new int[]{0,0};
-        return new int[]{map.lastKey(),map.firstKey()};
+        
+        int[] ans = new int[2];
+        if(pq.isEmpty()){
+            ans[0] = 0;
+            ans[1] = 0;
+        } else {
+            ans[1] = pq.peek();
+            PriorityQueue<Integer> pq2 = new PriorityQueue<>((o1,o2) -> o2-o1);
+            while(!pq.isEmpty()){
+                pq2.add(pq.poll());
+            }
+            ans[0] = pq2.peek();
+        }
+        
+        return ans;
     }
 }
