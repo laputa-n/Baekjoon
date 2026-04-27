e4793b9c4cfa5512a24ce1c3d6735fc13cb93ccf
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-04-27 12:21:01 +0900

    [level 2] Title: 가장 큰 수, Time: 186.29 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,14 +1,14 @@
 import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
-        String[] str = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            str[i] = String.valueOf(numbers[i]);
+        int size = numbers.length;
+        String[] input = new String[size];
+        for(int i = 0; i<size; i++){
+            input[i] = String.valueOf(numbers[i]);
         }
-        Arrays.sort(str, (o1, o2) -> (o2+o1).compareTo(o1+o2));
-        if(str[0].equals("0")){
-            return "0";
-        }
-        return String.join("",str);
+        Arrays.sort(input, (o1,o2) -> (o2+o1).compareTo(o1+o2));
+        if(input[0].equals("0")) return "0";
+        return String.join("",input);
+        
     }
 }
