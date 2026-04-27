2e247a703e3caa924d169a28dc6168401d00baae
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-04-27 12:48:31 +0900

    [level 2] Title: H-Index, Time: 0.77 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,12 +1,14 @@
 import java.util.*;
+// 0 1 3 5 6
+// 5 4 3 2 1
 class Solution {
     public int solution(int[] citations) {
-        int ans = 0;
+        int size = citations.length;
         Arrays.sort(citations);
-        int length = citations.length;
-        for(int i = 0; i<length; i++){
-            int h = length - i;
-            if(h <= citations[i]){
+        int ans = 0;
+        for(int i = 0; i<size; i++){
+            int h = size - i;
+            if(citations[i] >= h){
                 ans = h;
                 break;
             }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 74.5 MB, 시간: 1.75 ms
+메모리: 74.3 MB, 시간: 0.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 21일 21:24:53
+2026년 04월 27일 12:48:30
 
 ### 문제 설명
 
