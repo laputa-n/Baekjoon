b3daebd05bd25500344a27e0ba9e9c8b8ab5eaf9
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-04-28 12:02:56 +0900

    [level 1] Title: 최소직사각형, Time: 1.75 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,24 +1,16 @@
+import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
-        int w = sizes[0][0];
-        int h = sizes[0][1];
-        for(int i = 1; i < sizes.length; i++) {
-            if(w >= sizes[i][0] && h >= sizes[i][1]) continue;
-            if(w >= sizes[i][1] && h >= sizes[i][0]) continue;
-            int rightW = Math.max(w,sizes[i][0]);
-            int rightH = Math.max(h,sizes[i][1]);
-            int rollW = Math.max(w,sizes[i][1]);
-            int rollH = Math.max(h,sizes[i][0]);
-            int right = rightW * rightH;
-            int roll = rollW * rollH;
-            if(right > roll){
-                w = rollW;
-                h = rollH;
-            } else {
-                w = rightW;
-                h = rightH;
-            }
+        int w = 1;
+        int h = 1;
+        for(int[] size: sizes){
+            int width = Math.max(size[0], size[1]);
+            int height = Math.min(size[0], size[1]);
+            
+            w = Math.max(width, w);
+            h = Math.max(height,h);
         }
+        
         return w*h;
     }
 }
