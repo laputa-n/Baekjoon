77ded1ca3ecd14f1fb709c73ad7d8331c3eb2677
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-04-28 18:42:30 +0900

    [level 2] Title: 카펫, Time: 9.85 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,12 +1,16 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] ans = {0,0};
-        for(int h = 1; h <= Math.sqrt(yellow); h++){
-        //i는 노란 부분의 높이
-            if(yellow%h != 0) continue;
-            int w = yellow/h;
-            if(brown == 2*(w+2+h))
-                ans =  new int[]{w+2,h+2};
+        int[] ans = new int[2];
+        for(int i = 1; i<=yellow; i++){
+            if(yellow%i != 0) continue;
+            int w = Math.max(i,yellow/i);
+            int h = Math.min(i,yellow/i);
+            if(brown == (w + h + 2)*2){
+                ans[0] = w+2;
+                ans[1] = h+2;
+            }
         }
         return ans;
     }
