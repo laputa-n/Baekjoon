04c2af75046c1973a7a2893ce833ddb45b9440e0
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-04-29 13:07:25 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.75 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,47 +1,46 @@
 import java.util.*;
 class Solution {
-    static int[] parent;
+    static ArrayList<Integer>[] adj;
+    static int min;
     public int solution(int n, int[][] wires) {
-        int ans = Integer.MAX_VALUE;
-        parent = new int[n+1];
-
+        min = n;
+        adj = new ArrayList[n+1];
+        for(int i = 0; i<=n; i++){
+            adj[i] = new ArrayList<Integer>();
+        }
+        for(int[] wire: wires){
+            adj[wire[0]].add(wire[1]);
+            adj[wire[1]].add(wire[0]);
+        }
+        
         for(int i = 0; i<n-1; i++){
-            for(int j = 0; j < n+1; j++){
-                parent[j] = j;
+            if(min == 0) break;
+            int a = wires[i][0];
+            int b = wires[i][1];
+            adj[a].remove(Integer.valueOf(b));
+            adj[b].remove(Integer.valueOf(a));
+            
+            boolean[] visited = new boolean[n+1];
+            Queue<Integer> q = new LinkedList<>();
+            q.add(1);
+            visited[1] = true;
+            int cnt = 1;
+            while(!q.isEmpty()){
+                int cur = q.poll();
+                for(int next: adj[cur]){
+                    if(!visited[next]){
+                        q.add(next);
+                        visited[next] = true;
+                        cnt++;
+                    }
+                }
             }
-            for(int j = 0; j<n-1; j++){
-                if(i == j) continue;
-                union(wires[j][0],wires[j][1]);
-            }
-            Set<Integer> set = new HashSet<>();
-            for(int j = 1; j<n+1; j++){
-                set.add(find(j));
-            }
-            int num1 = (int) set.toArray()[0];
-            int num2 = (int) set.toArray()[1];
-            int cnt1 = 0;
-            int cnt2 = 0;
-            for(int j = 1; j<n+1; j++){
-                if(parent[j] == num1) cnt1++;
-                if(parent[j] == num2) cnt2++;
-            }
-            if(cnt1 == cnt2) return 0;
-            ans = Math.min(ans,Math.abs(cnt1 - cnt2));
+            min = Math.min(min, Math.abs(cnt - (n-cnt)));
+            
+            adj[a].add(b);
+            adj[b].add(a);
         }
-        return ans==Integer.MAX_VALUE?-1:ans;
-    }
-    static int find(int num){
-        if(parent[num] == num) return num;
-        return parent[num] = find(parent[num]);
-    }
-
-    static void union(int num1, int num2){
-        int root1 = find(num1);
-        int root2 = find(num2);
-        if(root1 < root2){
-            parent[root2] = root1;
-        } else {
-            parent[root1] = root2;
-        }
+        
+        return min;
     }
 }
