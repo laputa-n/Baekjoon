cfbab7f46157f740ebe2b1d03fcdc48b05a41c22
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-05-13 16:01:53 +0900

    [level 5] Title: 방의 개수, Time: 222.25 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.java
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.java
@@ -4,62 +4,41 @@ class Solution {
     static int[] dCol = {0,1,1,1,0,-1,-1,-1};
     
     public int solution(int[] arrows) {
-        Node cur = new Node(0,0);
+        int answer = 0;
         
-        Map<Node, List<Node>> visited = new HashMap<>();
+        // row,col
+        Set<String> visitedNode = new HashSet<>();
+        //r,c -> nextRow,nextCol
+        Set<String> visitedEdge = new HashSet<>();
         
-        int cnt = 0;
-        for(int d: arrows){
+        int curRow = 0;
+        int curCol = 0;
+        
+        visitedNode.add(curRow + "," + curCol);
+        
+        for(int dir: arrows){
             for(int i = 0; i<2; i++){
-                Node nextNode = new Node(cur.row + dRow[d], cur.col + dCol[d]);
+                int nextRow = curRow + dRow[dir];
+                int nextCol = curCol + dCol[dir];
                 
-                if(!visited.containsKey(nextNode)){
-                    visited.put(nextNode, makeEdgeList(cur));
-                    
-                    if(!visited.containsKey(cur)){
-                        visited.put(cur, makeEdgeList(nextNode));
-                    } else {
-                        visited.get(cur).add(nextNode);
-                    }
-                } else if(visited.containsKey(nextNode) && !visited.get(cur).contains(nextNode)){
-                    visited.get(nextNode).add(cur);
-                    visited.get(cur).add(nextNode);
-                    cnt++;
+                String nextNode = nextRow + "," + nextCol;
+                String edge = curRow + "," + curCol + "->" + nextRow + "," + nextCol;
+                String reverseEdge = nextRow + "," + nextCol + "->" + curRow + "," + curCol;
+                
+                if(visitedNode.contains(nextNode) && !visitedEdge.contains(edge)){
+                    answer++;
                 }
-                cur = nextNode;
-                    
+                
+                visitedNode.add(nextNode);
+                visitedEdge.add(edge);
+                visitedEdge.add(reverseEdge);
+                
+                curRow = nextRow;
+                curCol = nextCol;
             }
         }
         
-        return cnt;
-    }
-    
-    
-    static ArrayList<Node> makeEdgeList(Node n){
-        ArrayList<Node> edge = new ArrayList<>();
-        edge.add(n);
-        return edge;
-    }
-    static class Node {
-        int row, col;
-        
-        public Node(int r, int c){
-            this.row = r;
-            this.col = c;
-        }
-        
-        @Override
-        public boolean equals(Object o){
-            if(this == o) return true;
-            if(o == null || getClass() != o.getClass()) return false;
-            
-            Node node = (Node) o;
-            return row == node.row && col == node.col;
-        }
-        @Override
-        public int hashCode(){
-            return Objects.hash(row, col);
-        }
+        return answer;
     }
     
 }
