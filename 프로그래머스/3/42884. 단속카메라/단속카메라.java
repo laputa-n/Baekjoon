5bb756361d57108cd2622f90e435bb63573b3be6
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-05-15 17:24:08 +0900

    [level 3] Title: 단속카메라, Time: 4.26 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,24 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        Arrays.sort(routes, (r1, r2) -> r1[0] - r2[0]);
+        int carCnt = routes.length;
+        int cameraCnt = 1;
+        Arrays.sort(routes,(r1, r2) -> r1[0]-r2[0]);
         
         int left = routes[0][0];
         int right = routes[0][1];
         
-        int cnt = 0;
-        
-        for(int i = 1; i<routes.length; i++){
-            if(routes[i][0] <= right){
-                left = routes[i][0];
-                right = Math.min(right, routes[i][1]);
-            } else {
-                cnt++;
+        for(int i = 1; i<carCnt; i++){
+            if(right < routes[i][0]){
+                cameraCnt++;
                 left = routes[i][0];
                 right = routes[i][1];
+            } else {
+                left = routes[i][0];
+                right = Math.min(right, routes[i][1]);
             }
         }
         
-        return cnt+1;
+        return cameraCnt;
+        
+        
     }
 }
