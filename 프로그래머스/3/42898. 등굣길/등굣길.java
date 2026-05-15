f539ccbbc1f900ff0ac2ccc1869f774d0a0e1de0
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-05-15 16:41:59 +0900

    [level 3] Title: 등굣길, Time: 0.68 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,27 +1,36 @@
 class Solution {
-    
+    static final int INF =  1000000007;
     public int solution(int m, int n, int[][] puddles) {
-        final int INF = 1000000007;
-        //m: 가로, n: 세로
-        int[][] dp = new int[n+1][m+1];
-
-        for(int[] p: puddles){
-            dp[p[1]][p[0]] = -1;
+        int[][] map = new int[n][m];
+        
+        for(int[] puddle: puddles){
+            int col = puddle[0]-1;
+            int row = puddle[1]-1;
+            
+            map[row][col] = -1;
         }
-
-        dp[1][1] = 1;
-        for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=m; j++){
-                if(dp[i][j] == -1) continue;
-
-                int up = dp[i-1][j];
-                int left = dp[i][j-1];
-
-                if(up != -1) dp[i][j] += up%INF;
-                if(left != -1) dp[i][j] += left%INF;
+        
+        for(int i = 0; i<m; i++){
+            if(map[0][i] == -1) break;
+            map[0][i] = 1;
+        }
+        
+        for(int i = 0; i<n; i++){
+            if(map[i][0] == -1) break;
+            map[i][0] = 1;
+        }
+        
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<m; j++){
+                if(map[i][j] == -1) continue;
+                
+                if(map[i-1][j] != -1) map[i][j] += map[i-1][j]%INF;
+                if(map[i][j-1] != -1) map[i][j] += map[i][j-1]%INF;
             }
         }
-
-        return dp[n][m]%INF;
+        
+        return map[n-1][m-1]%INF;
+        
+        
     }
 }
