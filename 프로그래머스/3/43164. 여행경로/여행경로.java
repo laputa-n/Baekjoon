21e3862dc3475cfa0b0c89159363e55bd73247ea
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-05-19 18:34:14 +0900

    [level 3] Title: 여행경로, Time: 7.36 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,27 +1,30 @@
 import java.util.*;
 class Solution {
-    static boolean[] used;
-    static ArrayList<String> ans = new ArrayList<>();
+    static boolean[] visited;
+    static String ans;
+    static boolean flag = false;
     static String[] solution(String[][] tickets) {
-        used = new boolean[tickets.length];
-        Arrays.sort(tickets, (a,b) -> {
-            if(a[0].equals(b[0])) return a[1].compareTo(b[1]);
-            return a[0].compareTo(b[0]);
-        });
-        DFS(0,"ICN",tickets, "ICN");
-        return ans.get(0).split(" ");
+        Arrays.sort(tickets, (t1,t2) -> t1[1].compareTo(t2[1]));
+        visited = new boolean[tickets.length];
+        DFS("ICN", "ICN", tickets, 0);
+        ans = ans.trim();
+        return ans.split(" ");
+        
     }
-    static void DFS(int cnt, String city, String[][] tickets, String path){
-        if(cnt == tickets.length){
-            ans.add(path);
+    static void DFS(String cur, String path, String[][] tickets, int depth){
+        if(flag) return;
+        if(depth == tickets.length){
+            ans = path;
+            flag = true;
             return;
         }
-
         for(int i = 0; i<tickets.length; i++){
-            if(tickets[i][0].equals(city) && !used[i]){
-                used[i] = true;
-                DFS(cnt+1, tickets[i][1], tickets, path+" " +tickets[i][1]);
-                used[i] = false;
+            if(tickets[i][0].equals(cur)){
+                if(!visited[i]){
+                    visited[i] = true;
+                    DFS(tickets[i][1], path + " " + tickets[i][1], tickets, depth+1);
+                    visited[i] = false;
+                }
             }
         }
     }
