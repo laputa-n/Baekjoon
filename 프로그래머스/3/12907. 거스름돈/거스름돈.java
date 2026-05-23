ddd4a72e5eb86729a3f0a20fb45a6309b3124264
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-05-23 14:10:32 +0900

    [level 3] Title: 거스름돈, Time: 8.18 ms, Memory: 55.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,16 @@
+class Solution {
+    static final int INF = 1000000007;
+    public int solution(int n, int[] money) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        
+        for(int coin: money){
+            for(int i = coin; i<=n; i++){
+                dp[i] += dp[i-coin];
+                dp[i] %= INF;
+            }
+        }
+        
+        return dp[n];
+    }
+}
