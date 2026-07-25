6e1ed958e43125c725ae3f0835796eb3b8a87813
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-07-25 14:28:06 +0900

    [level 3] Title: 합승 택시 요금, Time: 41.54 ms, Memory: 68.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -3,43 +3,37 @@ import java.util.*;
 class Solution {
     static final int INF = 20000001;
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        //#1 중간 어느 곳까지 같이 가고, 거기서 각자 가기
-        //#2 처음부터 따로 가기
-        //#3 S->A->B
-        //#4 S->B->A
         int[][] dp = new int[n+1][n+1];
-        for(int i = 0; i<=n; i++){
+        
+        for(int i = 0; i<= n; i++){
             Arrays.fill(dp[i], INF);
             dp[i][i] = 0;
         }
         
         for(int[] fare: fares){
-            int start = fare[0];
-            int end = fare[1];
+            int c = fare[0];
+            int d = fare[1];
             int cost = fare[2];
-            
-            dp[start][end] = cost;
-            dp[end][start] = cost;
+            dp[c][d] = dp[d][c] = cost;
         }
         
         for(int k = 1; k<=n; k++){
             for(int i = 1; i<=n; i++){
                 for(int j = 1; j<=n; j++){
-                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
+                    dp[i][j] = Math.min(dp[i][k] + dp[k][j], dp[i][j]);
                 }
             }
         }
         
-        int case1 = Integer.MAX_VALUE;
+        //#1 출발지 -> A -> B
+        //#2 출발지 -> B -> A (위와 동일한 건 A-B, 출발지에서 A로 가냐, B로 가냐 차이)
+        //#3 출발지 -> 다른 곳에서 둘 다 내린 후, 각자 집까지
+        int answer = dp[a][b] + Math.min(dp[s][a], dp[s][b]);
         for(int i = 1; i<=n; i++){
-            if(i == s || i == a || i == b) continue;
-            case1 = Math.min(case1, dp[s][i] + dp[i][a] + dp[i][b]);
+            if(i == a || i == b) continue;
+            answer = Math.min(answer, dp[s][i] + dp[i][a] + dp[i][b]);
         }
         
-        int case2 = dp[s][a] + dp[s][b];
-        int case3 = dp[s][a] + dp[a][b];
-        int case4 = dp[s][b] + dp[b][a];
-        int answer = Math.min(Math.min(Math.min(case1,case2), case3), case4);
         return answer;
     }
 }
