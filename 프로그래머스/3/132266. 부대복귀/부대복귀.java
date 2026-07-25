db6e069f09f89d7d080c16fcb5a2df59934cc9f3
laputa-n <128452330+laputa-n@users.noreply.github.com>
2026-07-25 13:25:21 +0900

    [level 3] Title: 부대복귀, Time: 200.41 ms, Memory: 229 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -2,39 +2,59 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        int[] ans = new int[sources.length];
-        
+        //destination에서 sources로 가는 최단거리
         ArrayList<Integer>[] adj = new ArrayList[n+1];
         for(int i = 1; i<=n; i++){
             adj[i] = new ArrayList<>();
         }
-        
-        for(int[] road : roads){
-            adj[road[0]].add(road[1]);
-            adj[road[1]].add(road[0]);
+        for(int[] road: roads){
+            int a = road[0];
+            int b = road[1];
+            
+            adj[a].add(b);
+            adj[b].add(a);
         }
         
         int[] dp = new int[n+1];
         Arrays.fill(dp, Integer.MAX_VALUE);
-        
-        Queue<Integer> q = new LinkedList<>();
-        q.add(destination);
         dp[destination] = 0;
-        while(!q.isEmpty()){
-            int cur = q.poll();
-            for(int next: adj[cur]){
-                if(dp[cur] + 1 < dp[next]){
-                    dp[next] = dp[cur] + 1;
-                    q.add(next);
+        
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(destination, 0));
+        
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+            
+            if(dp[cur.to] < cur.dist) continue;
+            
+            for(int next: adj[cur.to]){
+                if(dp[next] >= dp[cur.to] + 1){
+                    dp[next] = dp[cur.to] + 1;
+                    pq.add(new Node(next, dp[next]));
                 }
             }
         }
         
+        int[] answer = new int[sources.length];
+        
         for(int i = 0; i<sources.length; i++){
-            int res = dp[sources[i]];
-            ans[i] = res == Integer.MAX_VALUE? -1: res;
+            answer[i] = dp[sources[i]] == Integer.MAX_VALUE? -1 : dp[sources[i]];
         }
         
-        return ans;
+        return answer;
+        
+    }
+    static class Node implements Comparable<Node> {
+        int to, dist;
+        
+        public Node(int to, int dist){
+            this.to = to;
+            this.dist = dist;
+        }
+        
+        @Override
+        public int compareTo(Node o){
+            return Integer.compare(this.dist, o.dist);
+        }
     }
 }
